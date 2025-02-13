1bd0373d1c88bd568d47ec4c1c9d9204e8741481
Rev4nchist <dkhay@rog-strix>
2025-02-13 05:52:24 -0600

    Remove UI8 template files to fix deployment issues

--- app/ui8-design-template/taskin-dashboard/taskin-dashboard/components/shared/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
-
-export function KeepThemeProvider({ children, ...props }: Readonly<ThemeProviderProps>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
